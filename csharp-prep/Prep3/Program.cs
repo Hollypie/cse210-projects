f86efcbaa583d3061533300c6d0babb36b840cec
Hollypie <hollybriggs1701e@gmail.com>
2024-09-16 16:51:55 -0600

    Added Prep3 Assignment which is a number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = true;
+        
+        while (playAgain == true)
+        {
+            int guessNumber = -1;
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int numberOfGuesses = 0;
+
+            while (!(guessNumber == magicNumber))
+            {
+                Console.Write("What is the guess number? ");
+                string guessNum = Console.ReadLine();
+                numberOfGuesses ++;
+                guessNumber = int.Parse(guessNum);
+                
+                if (magicNumber == guessNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {numberOfGuesses} times to figure it out.");
+                }
+                else if (magicNumber < guessNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else 
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                
+            }
+
+            
+            Console.WriteLine();
+            Console.Write("Would you like to play again? y or n: ");
+            string answer = Console.ReadLine();
+
+            if (answer == "n")
+            {
+                playAgain = false;
+                Console.WriteLine("Thank you for playing my guessing game.");
+            }
+            else
+            {
+                playAgain = true;
+            }
+
+        }
+        
+        
     }
 }
