2c8d1a4e6edab147fb1710e54ae654a3b49a2191
Hollypie <hollybriggs1701e@gmail.com>
2024-09-16 14:48:51 -0600

    Completed Prep2 assignment on displaying grade results

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        bool passed = true;
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int gradeNum = int.Parse(userGrade);
+        Console.WriteLine();
+        string letter = "U";
+
+        if (gradeNum > 90)
+        {
+            letter = "A";
+            passed = true;
+        }
+        else if (gradeNum > 80)
+        {
+            letter = "B";
+            passed = true;
+        }
+        else if (gradeNum > 70)
+        {
+            letter = "C";
+            passed = true;
+        }
+        else if (gradeNum > 60)
+        {
+            letter = "D";
+            passed = false;
+        }
+        else
+        {
+            letter = "F";
+            passed = false;
+        }
+
+        Console.Write($"You have a {letter}");
+
+        if (!(gradeNum < 60))
+        {
+            if (gradeNum % 10 >= 7)
+            {
+                Console.Write("+");
+            }
+            else if (gradeNum % 10 < 3)
+            {
+                Console.Write("-");
+            }
+
+        }
+        
+        Console.WriteLine(); 
+
+        if (passed == true)
+        {
+        
+            Console.WriteLine("Congradulations you have passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("You have not passed the class. You will need to retake this class. Better luck next time.");
+        }
     }
 }
