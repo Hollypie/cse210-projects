36a62f0c4825f5a3dfe8ff9a43597b8ac77f6959
Hollypie <hollybriggs1701e@gmail.com>
2024-10-18 11:06:40 -0600

    completed and added the Learning activity for week 6

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        Square square = new Square("blue", 5);
+        Rectangle rectangle = new Rectangle("green", 5, 6);        
+        Circle circle = new Circle("red", 5);
+        
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}.");
+            Console.WriteLine();
+        }
     }
 }
